9cb9daca2982ce47adbaa4404cb488e8a862aa3c
Kit <kittsville@googlemail.com>
2016-01-21 20:27:53 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# GifSound 2
+
+GifSound.com but better
+
+### Better How?
+
+- Syncs gif and sound start time
+- No server-side code
+- Modular gif/sound plugins (easy to add support for new gif/sound embeds)
+
+### Better Why?
+
+I spend too much time on [/r/GifSound](http://reddit.com/r/gifsound) and found it annoying that gifs sometimes didn't sync to the audio because of gif/YouTube loading times. I was going to contact gifsound.com's creators but I wanted to check if it was possible first. Ended up making my own version from scratch.
+
+### Contact
+
+Please direct suggestions, questions, sexy nudes and hate mail to kittsville(at)gmail.com or pester me on Twitter [(@kittsville)](https://twitter.com/kittsville) or message me on [Reddit](http://reddit.com/user/kittsville/).
+
+### License
+
+GPL v2 or later
---
+++ assets/scripts.js
@@ -0,0 +1,279 @@
+/*
+ * The primary objects of concern are:
+ * theGif		- Handles the embedded animation (gif, gifv, webm, etc.)
+ * theSound		- Handles the embedded audio (YouTube, SoundCloud, Vocaroo, mp3 file, etc.)
+ * theForm		- Handles the forms elements and its submission
+ * gifSound		- Handles the whole page and calling relevant objects from above
+ * If you think any of this sucks then please fork it.
+ * I continue to suck at JavaScript I just can't stop writing it.
+ */
+var theGif, theSound, theForm, gifSound;
+
+$(function(){
+
+theForm = {
+	s: {
+		form:			$('form#gifsound-input'),
+		gifInput: 		$('input#gif-url'),
+		soundInput: 	$('input#sound-url'),
+		startTimeInput:	$('input#sound-start-time'),
+		makeButton:		$('a#make-gifsound'),
+	},
+	
+	init: function() {
+		this.bindUIHandlers();
+	},
+	
+	bindUIHandlers : function() {
+		this.s.form.submit(function(event){
+			event.preventDefault();
+			
+			// Resets
+			gifSound.s.gifReady   = false;
+			gifSound.s.soundReady = false;
+			
+			theForm.processSoundURL(theForm.s.soundInput.val(), theForm.s.startTimeInput.val());
+			theForm.processGifURL(theForm.s.gifInput.val());
+		});
+	},
+	
+	processGifURL : function(gifURL) {
+		$.each(gifSound.s.gifPlugins, function(i, plugin) {
+			if (plugin.recogniseURL(gifURL)) {
+				theGif = plugin;
+				return true;
+			}
+		});
+		
+		if (typeof theGif === 'object') {
+			theGif.embedGif(gifURL, gifSound.s.gifWrapper);
+		} else {
+			throw "I don't have a media plugin that can handle that gif URL";
+		}
+	},
+	
+	processSoundURL : function(soundURL, startTime) {
+		startTime = parseInt(startTime);
+		
+		if (startTime === NaN) {
+			startTime = 0;
+		}
+		
+		$.each(gifSound.s.soundPlugins, function(i, plugin) {
+			if (plugin.recogniseURL(soundURL)) {
+				theSound = plugin;
+				return true;
+			}
+		});
+		
+		if (typeof theSound === 'object') {
+			theSound.embedSound(soundURL, gifSound.s.soundWrapper, startTime);
+		} else {
+			throw "I don't have a media plugin that can handle that audio URL";
+		}
+	},
+};
+
+/*
+ * SOUND PLUGINS
+ * All sound plugins must have the following methods:
+ * recogniseURL - Given a URL returns true or false if it can handle that URL
+ * embedSound   - Embed appropriate sound player (YouTube embed, <audio> tag, etc.) set to be PAUSED
+ * playSound    - Plays embedded sound
+ * pauseSound   - Pauses embedded sound
+ */
+
+YTPlugin = {
+	s : {
+		regex     : /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/, // http://stackoverflow.com/a/8260383/3565450
+		videoId   : '',	   // Video ID .e.g dQw4w9WgXcQ
+		startTime : 0,
+		wrapper   : false,
+		player    : false,
+		apiLoading: false, // Whether the YouTube IFrame Player API has started asynchronously loading
+		apiLoaded : false, // Whether the YouTube IFrame Player API has been loaded
+	},
+	
+	// Gets video ID of YouTube URL
+	getVideoID : function(url) {
+		return url.match(YTPlugin.s.regex)[1];
+	},
+	
+	// Checks if URL matches a YouTube video
+	recogniseURL : function(url) {
+		var match = url.match(YTPlugin.s.regex);
+		
+		if (match&&match[1].length == 11) {
+			YTPlugin.s.videoID = match[1];
+			return true;
+		} else {
+			return false;
+		}
+	},
+	
+	embedSound : function(url, wrapper, startTime) {
+		// Resets variable changed if video has been previously embedded
+		YTPlugin.s.firstPlay = true;
+		YTPlugin.s.player    = false;
+		
+		
+		YTPlugin.s.videoId   = YTPlugin.getVideoID(url);
+		YTPlugin.s.startTime = startTime;
+		YTPlugin.s.wrapper   = wrapper;
+		
+		if (YTPlugin.s.apiLoaded) {
+			YTPlugin.loadVideo();
+		} else if (!YTPlugin.s.apiLoading) {
+			YTPlugin.s.apiLoading = true;
+			
+			wrapper.html('<div id="youtube-embed"/>');
+			
+			var tag = document.createElement('script');
+
+			tag.src = "https://www.youtube.com/iframe_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			
+			window.onYouTubeIframeAPIReady = function() {
+				YTPlugin.s.apiLoaded  = true;
+				YTPlugin.s.apiLoading = false;
+				YTPlugin.loadVideo();
+			}
+		}
+	},
+	
+	// Loads video via YT API. Assumes API has been loaded
+	loadVideo : function() {
+		YTPlugin.s.player = new YT.Player('youtube-embed', {
+			height       : '300',
+			width        : '300',
+			startSeconds : YTPlugin.s.startTime,
+			videoId      : YTPlugin.s.videoId,
+			playerVars   : {
+				'autoplay' : 1,
+				'controls' : 0,
+			},
+			events       : {
+				'onReady'       : YTPlugin.onPlayerReady,
+				'onStateChange' : YTPlugin.playerStateChange,
+			},
+		});
+	},
+	
+	playerStateChange : function(event) {
+		if (event.data == YT.PlayerState.PLAYING && YTPlugin.s.firstPlay) {
+			YTPlugin.s.player.pauseVideo();
+			YTPlugin.s.firstPlay = false;
+			gifSound.soundReady();
+		}
+	},
+	
+	playSound : function() {
+		YTPlugin.s.player.playVideo();
+	},
+	
+	pauseSound : function() {
+		YTPlugin.s.player.pauseVideo();
+	},
+};
+
+/*
+ * GIF PLUGINS
+ * All gif plugins must have the following methods:
+ * recogniseURL - Given a URL returns true or false if it can handle that URL
+ * embedGif     - Embed appropriate gif player (Webm player, <img> tag, etc.) set to be PAUSED
+ * playGif      - Plays embedded gif
+ * pauseGif     - Pauses embedded gif
+ */
+
+/*
+ * General purpose gif plugin
+ * Embeds gifs with an img tag (wow, much difficult)
+ */
+GifPlugin = {
+	s : {
+		regex   : /.+/, // /^i\.imgur\.com\/[a-zA-Z0-9]{5,8}\.gif/
+		img 	: false,
+	},
+	
+	recogniseURL : function(url) {
+		var match = url.match(GifPlugin.s.regex);
+		
+		if (match) {
+			return true;
+		} else {
+			return false;
+		}
+	},
+	
+	embedGif : function(url, wrapper) {
+		GifPlugin.s.img = $('<img/>',{src:url});
+		
+		GifPlugin.s.img.hide();
+		
+		wrapper.html(GifPlugin.s.img);
+		
+		GifPlugin.s.img.one('load', gifSound.gifReady);
+		
+		/*
+		 * Cache fix for browsers that don't trigger 'load'. Thanks Nick Craver:
+		 * http://stackoverflow.com/a/2392448/3565450
+		 */
+		if (GifPlugin.s.img.complete) {
+			GifPlugin.s.img.trigger('load');
+		}
+	},
+	
+	playGif : function() {
+		GifPlugin.s.img.show();
+	},
+	
+	pauseGif : function() {
+		GifPlugin.s.img.hide();
+	},
+},
+
+gifSound = {
+	s : {
+		soundPlugins : [YTPlugin],
+		gifPlugins   : [GifPlugin],
+		soundWrapper : $('div#sound-wrapper'),
+		gifWrapper   : $('div#gif-wrapper'),
+		soundReady   : false,
+		gifReady     : false,
+	},
+	
+	init : function() {
+		this.bindUIHandlers();
+	},
+	
+	bindUIHandlers : function() {
+		
+	},
+	
+	soundReady : function() {
+		console.log('Sound Ready');
+		
+		gifSound.s.soundReady = true;
+		gifSound.playIfSynced();
+	},
+	
+	gifReady : function() {
+		console.log('Gif Ready');
+		
+		gifSound.s.gifReady = true;
+		gifSound.playIfSynced();
+	},
+	
+	playIfSynced : function() {
+		if (gifSound.s.gifReady && gifSound.s.soundReady) {
+			console.log('Gif and Sound are synced');
+			theGif.playGif();
+			theSound.playSound();
+		}
+	}
+};
+
+theForm.init();
+
+});
