kittsville/GifSound-2

commit ee04ccaa960ddd0aa692cf114993ab142c950849

Moved URL sanitization to method

assets/scripts.js
@@ -46,8 +46,12 @@ theForm = {
 		theForm.processGifURL(theForm.s.gifInput.val());
 	},
 	
+	sanitizeURL : function(url) {
+		return url.replace(theForm.s.urlSanitizer, '');
+	}
+	
 	processGifURL : function(gifURL) {
-		gifURL = gifURL.replace(theForm.s.urlSanitizer, '');
+		gifURL = theForm.sanitizeURL(gifURL);
 		
 		$.each(gifSound.s.gifPlugins, function(i, plugin) {
 			if (plugin.recogniseURL(gifURL)) {
@@ -65,7 +69,7 @@ theForm = {
 	},
 	
 	processSoundURL : function(soundURL, startTime) {
-		soundURL = soundURL.replace(theForm.s.urlSanitizer, '');
+		soundURL = theForm.sanitizeURL(soundURL);
 		
 		startTime = parseInt(startTime);
 		

commit b1e6a3d30a8c0ecfe1e77634ae1f91fc8b201eb4

Fixed syntax error
Introduced in ee04ccaa960ddd0aa692cf114993ab142c950849

assets/scripts.js
@@ -48,7 +48,7 @@ theForm = {
 	
 	sanitizeURL : function(url) {
 		return url.replace(theForm.s.urlSanitizer, '');
-	}
+	},
 	
 	processGifURL : function(gifURL) {
 		gifURL = theForm.sanitizeURL(gifURL);